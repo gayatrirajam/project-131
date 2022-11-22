3c7a42091406f07b535d5da7f64312096b4f91f8
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-11-21 21:20:24 -0600

    Add files via upload

---
+++ bikeandscooter.js
@@ -0,0 +1,23 @@
+var status = "";
+function preload(){
+    var bikeandscooter = loadImage("bikeandscooter.jpg");
+}
+function setup(){
+    canvas = createCanvas(600, 550);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Object";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
---
+++ plantandwateringcan.js
@@ -0,0 +1,23 @@
+var status = "";
+function preload(){
+    var plantandwateringcan = loadImage("plantandwateringcan.jpg");
+}
+function setup(){
+    canvas = createCanvas(600, 550);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Object";
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
